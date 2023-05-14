56f1da9c65db6a6984ed9b52de77ae9bac3c081d
Gabriel Varela <gv42@duke.edu>
2023-05-14 14:44:07 -0400

    Update the package name calling
    - Ran use_pipe(): created new utils-pipe.R file + updated DESCRIPTION and some .Rd files in /man
    
    - Updated qap_setup and qap_run so they explicitely call on every package. NOTE: still includes a call to magrittr

--- R/qap_run.R
+++ R/qap_run.R
@@ -1,16 +1,16 @@
 #' Quadratic Assignment Procedure (`qap_run`).
 #'
-#' @description The `qap_run` function is a wrapper around `sna`'s Quadratic Assignment Procedure models `netlm()` and `netlogit()`. It expects a networks objects containing dependent and independent variables of interest (e.g, the output from `qap_setup`).
+#' @description The `qap_run` function is a wrapper around `sna`'s Quadratic Assignment Procedure models `sna::netlm()` and `sna::netlogit()`. It expects a networks objects containing dependent and independent variables of interest (e.g, the output from `qap_setup`).
 #'
 #' @param net An `igraph` or `network` object.
 #' @param dependent A string naming the dependent variable of interest.
 #' @param variables A vector of strings naming the independent variables of interest.
 #' @param directed A logical statement identifying if the network should be treated as directed. Defaults to False.
 #' @param family A string identifying the functional form. Options are "linear" and "binomial". Defauts to "linear".
-#' @return `qap_run` returns a list of elements `model_results` that include: 
-#' 
+#' @return `qap_run` returns a list of elements `model_results` that include:
+#'
 #' - `covs_df`, a data frame containing term labels, estimates, standard errors and p-values
-#' 
+#'
 #' - `mods_df`, a data frame containing model-level information including the number of observations, AIC and BIC statistics.
 #' @export
 #'
@@ -22,53 +22,49 @@
 #'          family = "linear")
 
 qap_run <- function(net, dependent = NULL, variables, directed = F, family = "linear") {
-  
-  require(sna)
-  require(igraph)
-  require(intergraph)
-  require(tidygraph)
-  require(dplyr)
-  
+
+  require(magrittr)
+
   # Make sure it's an igraph object
   if ("network" %in% class(net)) {
-    net <- asIgraph(net)
+    net <- intergraph::asIgraph(net)
   }
-  
+
   # Get DV matrix
   if (is.null(dependent)) {
-    dv <- as.matrix(as_adjacency_matrix(net))
+    dv <- as.matrix(igraph::as_adjacency_matrix(net))
   } else {
-    dv <- as.matrix(as_adjacency_matrix(net, attr = dependent))}
-  
+    dv <- as.matrix(igraph::as_adjacency_matrix(net, attr = dependent))}
+
   # Get IV matrices
   ivs <- list()
   for (i in 1:length(variables)) {
     iv <- variables[[i]]
-    iv <- as.matrix(as_adjacency_matrix(net, attr = iv))
+    iv <- as.matrix(igraph::as_adjacency_matrix(net, attr = iv))
     ivs[[i]] <- iv
   }
-  
+
   # Run QAP
   if (directed == T) {mode = "digraph"} else {mode = "graph"}
   if (family == "binomial"){
     # if (all(dv %in% 0:1) == T){
-    res <- netlogit(dv, ivs, reps = 10, mode = mode)
+    res <- sna::netlogit(dv, ivs, reps = 10, mode = mode)
   } else if (family == "linear") {
-    res <- netlm(dv, ivs, reps = 100, mode = mode)
+    res <- sna::netlm(dv, ivs, reps = 100, mode = mode)
   } else {print("Not an available family -- Try 'linear' or 'binomial'")}
-  
+
   # Tidy results
   variables <- c("intercept", variables)
-  if (class(res) == "netlogit"){
-    covs_df <- tibble(covars = variables, estimate = res$coefficients, 
-                      `exp(estimate)` = exp(res$coefficients), se = res$se, 
-                      pvalue = res$pgreqabs) 
-    mods_df <- tibble(num_obs = res$n, aic = res$aic, bic = res$bic)
+  if (class(res) == "sna::netlogit"){
+    covs_df <- dplyr::tibble(covars = variables, estimate = res$coefficients,
+                      `exp(estimate)` = exp(res$coefficients), se = res$se,
+                      pvalue = res$pgreqabs)
+    mods_df <- dplyr::tibble(num_obs = res$n, aic = res$aic, bic = res$bic)
   } else {
-    covs_df <- tibble(covars = variables, estimate = res$coefficients, 
+    covs_df <- dplyr::tibble(covars = variables, estimate = res$coefficients,
                       se = res$se, pvalue = res$pgreqabs)
-    mods_df <- tibble(num_obs = res$n, aic = res$aic, bic = res$bic)
+    mods_df <- dplyr::tibble(num_obs = res$n, aic = res$aic, bic = res$bic)
   }
-  
+
   model_results <<- list(covs_df, mods_df)
 }
