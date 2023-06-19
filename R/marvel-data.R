d74658f1b8e3a53e5abdc4dfff6eeeb34973dd49
Gabriel Varela <gv42@duke.edu>
2023-06-18 21:03:05 -0700

    Update marvel-data.R

--- R/marvel-data.R
+++ R/marvel-data.R
@@ -1,26 +1,23 @@
 #' Character relationships among Marvel characters
 #'
-#' The nodes csv contains 107 different characters, 
-#' and the edges csv contains 353 weighted relationships between those characters, 
-#' which were calculated based on how many times two characters' names appeared 
-#' within 15 words of one another in the novel. 
-#'
 #' @docType data
 #'
 #' @usage data(marvel)
 #'
-#' @format two objects of class \code{"csv"}
+#' @format An object of class \code{"csv"}. Contains 107 different characters, and  353 weighted relationships
+#' between those characters, which were calculated based on how many times two characters' names appeared
+#' within 15 words of one another in the novel.
 #'
 #' @keywords datasets
 #'
-#' @references 
+#' @references
 #'
-#' @source Melanie Walsh (\href{https://github.com/melaniewalsh/sample-social-network-datasets/tree/master/sample-datasets/marvel}{Github}), 
-#' adapted from data originally compiled by Cesc Rosselló, Ricardo Alberich, 
+#' @source Melanie Walsh (\href{https://github.com/melaniewalsh/sample-social-network-datasets/tree/master/sample-datasets/marvel}{Github}),
+#' adapted from data originally compiled by Cesc Rosselló, Ricardo Alberich,
 #' and Joe Miro from Russ Chappell (\href{https://www.chronologyproject.com}{Website})
 #'
 #' @examples
 #' data(marvel)
-#' ideanet::netwrite(i_elements = edges$Source, j_elements = edges$Target, 
+#' ideanet::netwrite(i_elements = edges$Source, j_elements = edges$Target,
 #' weights = edges$Weight)
 "marvel"
