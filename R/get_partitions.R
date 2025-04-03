8e8285a460114dc0677bf525be789c6d79b01fe9
Tom Wolff <tom.wolff@northwestern.edu>
2025-04-03 11:27:21 -0500

    Starting merge of Peter's CHAMP work to Tom's changes

--- R/get_partitions.R
+++ R/get_partitions.R
@@ -35,14 +35,16 @@ get_partitions <- function( network,
                             n_runs=100,
                             n_iterations=2, #parameter for cluster_leiden
                             seed=NULL,
-                            add_comm_detect=T ) {
+                            add_comm_detect=TRUE) {
+
+  # browser()
 
   # Check input network is undirected
   if (igraph::is_directed(network)) {stop("Input is directed. Only undirected networks are currently supported.")}
-    
+
   # If seed defined, use it to set the random seed for reproducibility
   if (!is.null(seed)) {set.seed(seed)}
-  
+
   # If gamma_range has more than two values, treat as a list of desired gamma
   # resolution parameter values. Otherwise, generate uniform random collection
   # of gamma values between the min (>=0) and max (>=1) value. If there is only one value in
@@ -53,29 +55,29 @@ get_partitions <- function( network,
     gamma = runif(n_runs,max(min(gamma_range),0),max(gamma_range,1))
   } else {gamma = gamma_range}
   #print(gamma)
-  
+
   # Call cluster_leiden multiple times, saving results to partitions
   nc <- vector("numeric",length(gamma))
-  partitions <- list() 
+  partitions <- list()
   for (i in 1:length(gamma)) {
     gc <- igraph::cluster_leiden(network, objective_function = "modularity",
-                                 resolution = gamma[i], 
+                                 resolution = gamma[i],
                                  n_iterations = n_iterations)
-    gc$gamma <- gamma[i] 
-    # cluster number 
+    gc$gamma <- gamma[i]
+    # cluster number
     cn <- length(gc)
-    cur <- partitions[cn] 
+    cur <- partitions[cn]
     # if there are no current partitions for this cn
     if (is.na(cur) || is.null(cur[[1]])) {
-      partitions[[cn]] <- list(gc) 
+      partitions[[cn]] <- list(gc)
     } else {
-      cur <- partitions[[cn]] 
+      cur <- partitions[[cn]]
       partitions[[cn]][[length(cur)+1]] <- gc
     }
-    nc[i] <- cn 
+    nc[i] <- cn
     #partitions[[cn]]
   }
-  
+
   # Now we fold in the comm_detect() results, running comm_detect() if
   # add_comm_detect is True, or directly using provided comm_detect() output
   if (is.logical(add_comm_detect)) {
@@ -85,43 +87,43 @@ get_partitions <- function( network,
       dev.off()
     }
   }
-  if (min(c("memberships", "summaries", "score_comparison") %in% 
+  if (min(c("memberships", "summaries", "score_comparison") %in%
           names(add_comm_detect)) == 1) {
-    membs <- add_comm_detect$memberships #We only need 
+    membs <- add_comm_detect$memberships #We only need
     for (i in 2:ncol(membs)) {
       memb <- membs[,i]
       #if (min(memb)==0) {memb <- memb + 1} #Some comm_detect() memberships count from 0?
       names(memb) <- igraph::V(network)$name
-      gc <- igraph::make_clusters(network, membership=memb, 
+      gc <- igraph::make_clusters(network, membership=memb,
                                   algorithm=colnames(membs)[i], modularity = F)
-      gc$gamma <- 0 #I don't think this gets used later, but setting for consistency 
-      # cluster number 
+      gc$gamma <- 0 #I don't think this gets used later, but setting for consistency
+      # cluster number
       cn <- length(gc)
-      cur <- partitions[cn] 
+      cur <- partitions[cn]
       # if there are no current partitions for this cn
       if (is.na(cur) || is.null(cur[[1]])) {
-        partitions[[cn]] <- list(gc) 
+        partitions[[cn]] <- list(gc)
       } else {
-        cur <- partitions[[cn]] 
+        cur <- partitions[[cn]]
         partitions[[cn]][[length(cur)+1]] <- gc
       }
       nc[length(gamma)-1+i] <- cn #This is bad form to be increasing the length of this vector
     }
   }
-  
+
   # unique_partitions filters out duplicate partitions
   partitions <- unique_partitions(partitions)
-  
+
   # best_partitions processes the output of unique_partitions to format it
-  # appropriately for the following steps. The function can also remove 
-  # partitions that were only generated less than min times (but we take min=1, 
+  # appropriately for the following steps. The function can also remove
+  # partitions that were only generated less than min times (but we take min=1,
   # effectively removing this functionality)
   partitions <- best_partitions(partitions$count, partitions$partitions, min=1)
-  
+
   # Save gamma min & max used so they can be used in CHAMP.R
   partitions$gamma_min <- min(gamma_range)
   partitions$gamma_max <- max(gamma_range)
-  
+
   cat('\n')
   if (is.logical(add_comm_detect)) { #At this point, if it is a logical, it's False
     print(paste(length(partitions$partitions),
@@ -130,24 +132,24 @@ get_partitions <- function( network,
     print(paste(length(partitions$partitions),
                 "unique partitions generated between cluster_leiden() and comm_detect()"))
   }
-  
+
   return(partitions)
-  
+
 }
 
 ###################################
 
-# Given a list of partitions, finds the unique ones 
+# Given a list of partitions, finds the unique ones
 # and the number of times each unique partition appeared.
 unique_partitions <- function(partitions) {
-  # number of distinct partitions 
-  partition_count <- vector("list", length = length(partitions)) 
+  # number of distinct partitions
+  partition_count <- vector("list", length = length(partitions))
   # which partitions are distinct
   distinct_partitions <- vector("list", length = length(partitions))
   for (i in 1:length(partitions)) { #The first index is the number of communities
     clustersi <- partitions[[i]]
     if (!is.null(clustersi)) { #Only proceed if found a partition with this number
-      partition_count[i] <- list(rep(1,length(clustersi))) 
+      partition_count[i] <- list(rep(1,length(clustersi)))
       distinct_partitions[[i]] <- clustersi
       # only want to iterate if length of clustersi is greater than 1 (that is, found more than once)
       if (length(clustersi)>1) {
@@ -156,7 +158,7 @@ unique_partitions <- function(partitions) {
           if (partition_count[[i]][j]!=0) {
             for (k in length(clustersi):(j+1)) {
               # if cluster k is not a duplicate
-              if (partition_count[[i]][k]!=0) { 
+              if (partition_count[[i]][k]!=0) {
                 cmp <- igraph::compare(clustersi[[j]],clustersi[[k]])
                 if (cmp==0) {
                   partition_count[[i]][k] <- 0
@@ -173,7 +175,7 @@ unique_partitions <- function(partitions) {
       }
     }
   }
-  
+
   # clean up distinct_partitions and partition_count (remove 0s and NAs)
   for (i in 1:length(distinct_partitions)) {
     if (!is.null(distinct_partitions[[i]])) {
@@ -181,7 +183,7 @@ unique_partitions <- function(partitions) {
       partition_count[[i]] <- partition_count[[i]][partition_count[[i]]!=0] # eliminate zeros
     }
   }
-  
+
   return(list("count" = partition_count, "partitions" = distinct_partitions))
 }
 
@@ -203,5 +205,5 @@ best_partitions <- function(c, p, min){
       }
     }
   }
-  return(list("count" = final_count, "partitions" = final_partitions)) 
+  return(list("count" = final_count, "partitions" = final_partitions))
 }
