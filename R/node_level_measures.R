87198e63b6eaa0fe8895421fd29e6bc58d9f5b80
tom.wolff@northwestern.edu <tomwolff@Toms-Mac-mini.local>
2026-02-03 15:14:42 -0600

    Storing tentative fixes to bonacich issue while awaiting Jim's feedback

--- R/node_level_measures.R
+++ R/node_level_measures.R
@@ -817,6 +817,8 @@ bonacich <- function(matrix, bpct = .75, directed = FALSE) {
     }
 
     maxev <- igraph::eigen_centrality(temp_g, directed = directed)$value
+    # maxev <- max(eigen(matrix)$values)
+    # maxev <- RSpectra::eigs(matrix, k = 1)$values
 
     # The ones we want are in the first column
     # Something's weird here -- it's combining the two columns that SAS outputs into a single value
